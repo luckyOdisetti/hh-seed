0dda3f65b951ce9dfdb2e55dffc9151df206340c
Lucky Odisetti <lucky.odisetti@gmail.com>
2023-03-24 03:04:01 -0700

    sig utils

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -7,14 +7,19 @@ pragma solidity ^0.8.9;
 contract Lock {
     uint public unlockTime;
     address payable public owner;
+    bool private _lock;
+    modifier noReEntry() {
+        if (_lock == true) revert NoReEntry();
+        _lock = true;
+        _;
+        _lock = false;
+    }
 
     event Withdrawal(uint amount, uint when);
+    error NoReEntry();
 
     constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
+        require(block.timestamp < _unlockTime, "time should be in the future");
 
         unlockTime = _unlockTime;
         owner = payable(msg.sender);
@@ -30,5 +35,25 @@ contract Lock {
         emit Withdrawal(address(this).balance, block.timestamp);
 
         owner.transfer(address(this).balance);
+    }
+
+    function _ethSignedMessage(
+        bytes32 _messageHash
+    ) internal pure returns (bytes32) {
+        return
+            keccak256(
+                abi.encode("\x19Ethereum Signed Message:\n32", _messageHash)
+            );
+    }
+
+    function _split(
+        bytes memory _sig
+    ) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(_sig.length == 65, "Invalid Signature Length");
+        assembly {
+            r := mload(add(_sig, 32))
+            s := mload(add(_sig, 64))
+            v := byte(0, mload(add(_sig, 96)))
+        }
     }
 }
